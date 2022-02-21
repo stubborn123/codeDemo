c43996f8d1378e63dc9e498b202c12cd5e04ab4c
zhangpeng <31369372+stubborn123@users.noreply.github.com>
2022-02-21 14:48:14 +0800

    关于springboot静态文件导入
    spring静态文件导入演示和代码

---
+++ zpspringboot3/src/main/java/com/example/zpspringboot3/Zpspringboot3Application.java
@@ -0,0 +1,13 @@
+package com.example.zpspringboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zpspringboot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zpspringboot3Application.class, args);
+    }
+
+}
