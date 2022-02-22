9f1093b1c3f42e763051127b71bf2eca3e9c9ff5
stubborn123 <31369372+stubborn123@users.noreply.github.com>
2022-02-23 03:10:13 +0800

    spring security整合

---
+++ zpspringsecurity/src/main/java/com/example/zpspringsecurity/ZpspringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.zpspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZpspringsecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZpspringsecurityApplication.class, args);
+    }
+
+}
---
+++ zpspringsecurity/src/test/java/com/example/zpspringsecurity/ZpspringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zpspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZpspringsecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
